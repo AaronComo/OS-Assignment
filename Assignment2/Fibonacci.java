331436b661e7a18601c0ad9bfaea55c1b8fc2b5b
AaronComo <107360494+AaronComo@users.noreply.github.com>
2023-03-17 22:33:59 +0800

    change dp to private.

--- Assignment2/Fibonacci.java
+++ Assignment2/Fibonacci.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Fibonacci extends Thread {
-    static int[] dp;
+    private static int[] dp;
 
     Fibonacci(int len) {
         dp = new int[len];
@@ -10,8 +10,7 @@ public class Fibonacci extends Thread {
     @Override
     public void run() {
         super.run();
-        dp[0] = 0;
-        dp[1] = 1;
+        dp[0] = 0; dp[1] = 1;
         for (int i = 2; i < dp.length; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
